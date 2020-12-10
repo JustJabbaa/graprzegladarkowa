ccbf76383c432a0b1d8d3b4763c2301759810c68
JustJabbaa <72550781+JustJabbaa@users.noreply.github.com>
2020-12-10 13:56:21 +0100

    1.01

--- class/Village.class.php
+++ class/Village.class.php
@@ -1,12 +1,15 @@
 <?php
 class Village 
 {
+    private $gm;
     private $buildings;
     private $storage;
     private $upgradeCost;
 
-    public function __construct()
+    public function __construct($gameManger)
     {
+        $this->gm = $gameManger;
+        $this->log('Utworzono nową wioskę', 'info');
         $this->buildings = array(
             'townHall' => 1,
             'woodcutter' => 0,
@@ -19,56 +22,69 @@ class Village
             'stone' => 0,
         );
         $this->upgradeCost = array( //tablica wszystkich budynkow
-            'woodcutter' => array(
-                1 => array(
-                    'wood' => 200
+                'woodcutter' => array(
+                    1 => array(
+                        'wood' => 50
+                    ),
+                    2 => array(
+                        'wood' => 250,
+                    ),
+                    3 => array(
+                        'wood' => 1500,
+                        'stone' => 500,
+                    ),
+                    4 => array(
+                        'wood' => 12000,
+                        'stone' => 250,
+                        'iron' => 50
+                    ),
+                    5 => array(
+                        'wood' => 72000,
+                        'stone' => 1000,
+                        'iron' => 300,
+                    )
                 ),
-                2 => array(
-                    'wood' => 150,
+                'stoneMine' => array(
+                    1 => array(
+                        'wood' => 1000,
+                    ),
+                    2 => array(
+                        'wood' => 3000,
+                        'stone' => 1000,
+                    ),
+                    3 => array(
+                        'wood' =>14000,
+                        'stone' => 700,
+                        'iron' => 250
+                    ),
+                    4 => array(
+                        'wood' => 40000,
+                        'stone' => 2000,
+                        'iron' => 500,
+                    ),
+                    5 => array(
+                        'wood' => 120000,
+                        'stone' => 10000,
+                        'iron' => 2000,
+                    )
                 ),
-                3 => array(
-                    'wood' => 900,
-                    'stone' => 50,
+                'ironMine' => array(
+                    1 => array(
+                        'wood' => 100,
+                    ),
+                    2 => array(
+                        'wood' => 300,
+                        'iron' => 100,
+                    )
                 ),
-                4 => array(
-                    'wood' => 7200,
-                    'stone' => 250,
-                    'iron' => 50
-                ),
-                5 => array(
-                    'wood' => 72000,
-                    'stone' => 1000,
-                    'iron' => 300,
-                )
-            ),
-            'stoneMine' => array(
-                1 => array(
-                    'wood' => 600,
-                ),
-                2 => array(
-                    'wood' => 3000,
-                    'stone' => 150,
-                ),
-                3 => array(
-                    'wood' =>20000,
-                    'stone' => 4343,
-                )
-            ),
-            'ironMine' => array(
-                1 => array(
-                    'wood' => 100,
-                ),
-                2 => array(
-                    'wood' => 300,
-                    'iron' => 100,
-                )
-            ),
+        
+
         );
     }
     private function woodGain(int $deltaTime) : float
     {
         //liczymy zysk na godzine z wzoru poziom_drwala ^ 2
-        $gain = $this->buildings['woodcutter']* 2 * 1500 ;
+        $gain = $this->buildings['woodcutter']* 2 * 750 ;
         // liczymy zysk na sekunde (godzina/3600)
         $perSecondGain = $gain / 3600;
         //zwracamy zysk w czasie $deltaTime
@@ -77,7 +93,7 @@ class Village
     private function ironGain(int $deltaTime) : float
     {
         //liczymy zysk na godzine z wzoru poziom_drwala ^ 2
-        $gain = $this->buildings['ironMine']*2 * 5000;
+        $gain = $this->buildings['ironMine']*2 * 125;
         // liczymy zysk na sekunde (godzina/3600)
         $perSecondGain = $gain / 3600;
         //zwracamy zysk w czasie $deltaTime
@@ -86,7 +102,7 @@ class Village
     private function stoneGain(int $deltaTime) : float
     {
         //liczymy zysk na godzine z wzoru poziom_drwala ^ 2
-        $gain = $this->buildings['stoneMine']*2 * 5000;
+        $gain = $this->buildings['stoneMine']*2 * 500;
         // liczymy zysk na sekunde (godzina/3600)
         $perSecondGain = $gain / 3600;
         //zwracamy zysk w czasie $deltaTime
@@ -114,6 +130,18 @@ class Village
         }
         //podnies lvl budynku o 1
         $this->buildings[$buildingName] += 1; 
+        return true;
+    }
+    public function checkBuildingUpgrade(string $buildingName) : bool
+    {
+        $currentLVL = $this->buildings[$buildingName];
+        $cost = $this->upgradeCost[$buildingName][$currentLVL+1];
+        foreach ($cost as $key => $value) {
+            //key - nazwa surowca
+            //value koszt surowca
+            if($value > $this->storage[$key])
+                return false;
+        }
         return true;
     }
     public function showHourGain(string $resource) : string
@@ -148,6 +176,24 @@ class Village
     {
         return $this->buildings[$building];
     }
-    
+    public function capacity(string $resource) : int 
+    {
+        switch ($resource) {
+            case 'wood':
+                return $this->woodGain(60*60*24); //doba
+                break;
+            case 'iron':
+                return $this->ironGain(60*60*12); //12 godzin
+                break;
+                
+            default:
+                return 0;
+                break;
+        }
+    }
+    public function log(string $message, string $type)
+    {
+        $this->gm->l->log($message, 'village', $type);
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-<?php 
-        require('./class/Village.class.php');
+<?php
+require_once('./class/GameManager.class.php');
         session_start();
-        if(!isset($_SESSION['v'])) // jeżeli nie ma w sesji naszej wioski
+        if(!isset($_SESSION['gm'])) // jeżeli nie ma w sesji naszej wioski
         {
-            echo "Tworzę nową wioskę...";
-            $v = new Village();
-            $_SESSION['v'] = $v;
-            //reset czasu od ostatniego odświerzenia strony
-            $deltaTime = 0;
+            $gm = new GameManager();
+            $_SESSION['gm'] = $gm;
         } 
         else //mamy już wioskę w sesji - przywróć ją
         {
-            $v = $_SESSION['v'];
-            //ilosc sekund od ostatniego odświerzenia strony
-            $deltaTime = time() - $_SESSION['time'];
+            $gm = $_SESSION['gm'];
         }
-        $v->gain($deltaTime);
+        $v = $gm->v; //neizależnie cyz nowa gra czy załadowana
+        $gm->sync(); //przelicz surowce
         
         if(isset($_REQUEST['action'])) 
         {
@@ -40,7 +36,6 @@
 
 
 
-        $_SESSION['time'] = time();
         
         
     ?>
@@ -68,20 +63,39 @@
         <div id="dol">
             <p>
                 <a class="button" href="index.php?action=upgradeBuilding&building=woodcutter">Rozbuduj drwala</a>
-                <a class="button" href="index.php?action=upgradeBuilding&building=stoneMine">Kopalnia Kamieni</a>
-                <a class="button" href="index.php?action=upgradeBuilding&building=ironMine">Kopalnia Żelaza</a>
-                <a class="button" href="www.google.com">XD?</a>
-                <a class="button" href="www.google.com">XD?</a>
-                <a class="button" href="www.google.com">XD?</a>
-                <a class="button" href="www.google.com">XD?</a> 
-                <a class="button" href="index.php">Odświerz strone</a>
-                <div class="dropdown">
-                    <button class="dropbtn">Dropdown</button>
-                    <div class="dropdown-content">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-                </div>
+                <a class="button" href="index.php?action=upgradeBuilding&building=stoneMine">Ulepsz Kopalnie Kamieni</a>
+                <a class="button" href="index.php?action=upgradeBuilding&building=ironMine">Ulepsz Kopalnie Żelaza</a>
+                <a class="button" href="www.google.com">.</a>
+                <a class="button" href="www.google.com">.</a>
+                <a class="button" href="www.google.com">.</a>
+                <a class="button" href="www.google.com">.</a> 
+                <a class="button" href="index.php">Odśwież strone</a>
+                <footer class="row">
+            <div class="col-12">
+            <table class="table table-bordered">
+            <?php
+            
+                
+                    
+                
+            
+            foreach ($gm->l->getLog() as $entry) {
+                $timestamp = date('d.m.Y H:i:s', $entry['timestamp']);
+                $sender = $entry['sender'];
+                $message = $entry['message'];
+                $type = $entry['type'];
+                echo "<tr>";
+                echo "<td>$timestamp</td>";
+                echo "<td>$sender</td>";
+                echo "<td>$message</td>";
+                echo "<td>$type</td>";
+                echo "</tr>";
+            }
+            
+            ?>
+            </table>
+            </div>
+        </footer>
         </div>
             </p>
 
@@ -101,7 +115,7 @@
         </footer>
         </div>
     </div>
-    <div id="prawo"></div>
+    <div id="prawo"><img src="grafika.jpg" style="width:100%;height:100%"></div>
     <div></div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
